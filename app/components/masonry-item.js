133c71f6f863a9c2902631a0e43eaa1911a8d5e2
Jordan Hawker <hawker.jordan@gmail.com>
2021-02-17 18:42:52 -0800

    Ensure layout always waits for images to load

--- app/components/masonry-item.js
+++ app/components/masonry-item.js
@@ -1,12 +1,29 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { Promise } from 'rsvp';
 
 export default class MasonryItemComponent extends Component {
   @tracked onLoad;
 
+  @tracked resolveLoad;
+
+  constructor() {
+    super(...arguments);
+
+    if (this.args.async) {
+      this.onLoad = new Promise((resolve) => {
+        this.resolveLoad = resolve;
+      });
+    }
+  }
+
   @action
   setLoadingPromise(onLoad) {
-    this.onLoad = onLoad;
+    onLoad.then((item) => {
+      if (this.resolveLoad) {
+        this.resolveLoad(item);
+      }
+    });
   }
 }
